068f8c8ca1a1a1bed4d929ca2440acfec6c42776
Nandhakumar.S <nandhudeveloper03@gmail.com>
2025-10-24 11:56:48 +0530

    Update Client Model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,8 +3,52 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Client extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = [
+        'client_name',
+        'user_id',
+        'client_email',
+        'client_phone',
+        'alternate_phone',
+        'address',
+        'date_of_birth',
+        'gender',
+        'marital_status',
+        'cibil_score',
+        'lead_by',
+        'is_active',
+        'is_deleted',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function agent()
+    {
+        return $this->belongsTo(Agent::class, 'lead_by');
+    }
+
+    public function loans()
+    {
+        return $this->hasMany(Loan::class);
+    }
+
+    public function supportTickets()
+    {
+        return $this->hasMany(SupportTicket::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
+    
 }
+
