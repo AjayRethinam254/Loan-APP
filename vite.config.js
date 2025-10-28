3c2722fcbd0e6311d0291b3d7afb143cab1af9a5
AjayRethinam254 <ajaydk254@gmail.com>
2025-10-28 12:11:30 +0530

    untracked files

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/main.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  plugins: [
+    vue(),
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/main.js'],
+      refresh: true,
+    }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'resources/js'),
+      '@core': path.resolve(__dirname, 'resources/js/core'),
+      '@styles': path.resolve(__dirname, 'resources/styles'),
+      '@core-scss': path.resolve(__dirname, 'resources/scss/core'),
+    },
+  },
 });
